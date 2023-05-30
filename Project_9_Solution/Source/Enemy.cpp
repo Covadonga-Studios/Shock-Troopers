d01b74c666920452ba1d545ed43208239bbfa898
Noelswag <125265329+Noelswag@users.noreply.github.com>
2023-05-30 23:01:28 +0200

    Fixed Draw function in Enemy.cpp
    It tried to access Bool Mirror before checking if it was Nullptr

--- Project_9_Solution/Source/Enemy.cpp
+++ Project_9_Solution/Source/Enemy.cpp
@@ -92,22 +92,9 @@ int Enemy::GetTargetDir(float dx, float dy)
 
 void Enemy::Draw()
 {
-	bool mirror = currentAnim->GetMirror();
-	bool mirror2 = currentAnim2->GetMirror();
-
-	if (currentAnim2 != nullptr)
-		if (mirror == false)
-		{
-			App->render->Blit(texture2, position.x + offsettexture2x, position.y + offsettexture2y, &(currentAnim2->GetCurrentFrame()));
-		}
-		else 
-		{
-			App->render->BlitMirror(texture, position.x + offsettexture1x, position.y + offsettexture1y, &(currentAnim->GetCurrentFrame()));
-		}
 	
-
 	if (currentAnim != nullptr)
-		if (mirror == true)
+		if (currentAnim->GetMirror())
 		{
 			App->render->BlitMirror(texture, position.x + offsettexture1x, position.y + offsettexture1y, &(currentAnim->GetCurrentFrame()));
 
@@ -118,7 +105,16 @@ void Enemy::Draw()
 			App->render->Blit(texture, position.x + offsettexture1x, position.y + offsettexture1y, &(currentAnim->GetCurrentFrame()));
 		}
 	
+	if (currentAnim2 != nullptr)
+		if (currentAnim2->GetMirror())
+		{
+			App->render->BlitMirror(texture, position.x + offsettexture1x, position.y + offsettexture1y, &(currentAnim->GetCurrentFrame()));
 
+		}
+		else
+		{
+			App->render->Blit(texture2, position.x + offsettexture2x, position.y + offsettexture2y, &(currentAnim2->GetCurrentFrame()));
+		}
 	
 }
 
